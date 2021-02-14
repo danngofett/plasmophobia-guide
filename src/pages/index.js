71a645b898e3f64338de7354f3028c26a060c8d2
Daniel Ngo <hello@danielno.com>
2021-02-15 01:30:31 +1100

    chore: restyled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,70 @@
 import React, { useState } from "react"
 import Ghosts from '@data/ghosts'
 import Evidence from '@data/evidence'
+import { css } from '@emotion/react'
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+const pageStyles = css`
+  color: #232129;
+  font-family: -apple-system, Roboto, sans-serif, serif;
+  margin: 0 auto;
+  max-width: 1366px;
+  padding: 20px;
 
-const ghostCardGrid = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)',
-  gap: '20px',
-}
+  @media (min-width: 768px) {
+    padding: 40px;
+  }
 
-const ghostCard = {
-  border: 'thin solid #232129',
-  padding: '20px'
-}
+  @media (min-width: 1280px) {
+    padding: 60px;
+  }
+`
+
+const ghostCardGrid = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 20px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media (min-width: 1280px) {
+    grid-template-columns: repeat(6, 1fr);
+  }
+`
+
+const ghostCard = css`
+  border: thin solid #232129;
+  padding: 20px;
+`
+
+const evidenceCardGrid = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  gap: 20px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`
+
+const evidenceCardGridItem = css`
+  padding: 0;
+`
+
+const evidenceCard = css`
+  border: thin solid #232129;
+  display: block;
+  padding: 20px;
+  width: 100%;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
 
 const IndexPage = () => {
   const [activeEvidence, setActiveEvidence] = useState([])
@@ -88,56 +135,55 @@ const IndexPage = () => {
    * Template.
    */
   return (
-    <main style={pageStyles}>
+    <main css={pageStyles}>
       <title>Plasmophobia Hunter Guide</title>
 
       <h1>Plasmophobia Hunter Guide</h1>
       <p>A pocket guide to hunt down a ghost without going through the intuitive journal currently. This guide is also mobile friendly - so you can bookmark this to your phone whilst you play.</p>
 
       <div>
-        <h2>Active Evidence</h2>
-        {
-          activeEvidence.length === 0
-          ? (<p>There are currently no active evidence.</p>)
-          : (
-            <ul>
-              {activeEvidence.map((item, index) => (
-                <li key={index}>
-                  <span>{item.name}</span>
-                  <button onClick={() => handleEvidenceRemove({ id: item.id })}>
-                    Remove
-                  </button>
-                </li>))}
-            </ul>)
-        }
-      </div>
-
-      <div>
-        <h2>Possible Ghosts</h2>
-        <div style={ghostCardGrid}>
-          {getAvailableGhost.length === 0 ? (
-            <p>There are no ghosts available found with this evidence!</p>
-          ) : getAvailableGhost.map((item) => (
-            <div style={ghostCard} key={item.name}>
-              <h3>{item.name}</h3>
-              <div dangerouslySetInnerHTML={{ __html: getHighlightedEvidence(item.evidence) }} />
-            </div>
-          )
-          )}
+        <div>
+          <h2>Evidence</h2>
+          <button onClick={() => setActiveEvidence([])}>Reset</button>
         </div>
-      </div>
 
-      <div>
-        <h2>Evidence</h2>
-        <ul>
+        <ul css={evidenceCardGrid}>
           {Evidence.data.map(({ id, name }) => (
-            <li key={id}>
-              <button onClick={() => handleEvidenceUpdate({ id, name })}>
+            <li
+              css={evidenceCardGridItem}
+              key={id}
+            >
+              <button
+                css={evidenceCard}
+                onClick={() => handleEvidenceUpdate({ id, name })}
+              >
                 { name }
               </button>
             </li>
           ))}
         </ul>
+      </div>
+
+      <div>
+        <h2>Possible Ghosts</h2>
+        <div css={ghostCardGrid}>
+          {getAvailableGhost.length === 0
+            ? (<p>There are no ghosts available found with this evidence!</p>)
+            : getAvailableGhost.map((item) =>
+            (
+              <div
+                css={ghostCard}
+                key={item.name}
+              >
+                <h3>{item.name}</h3>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: getHighlightedEvidence(item.evidence)
+                  }} />
+              </div>
+            )
+          )}
+        </div>
       </div>
     </main>
   )
